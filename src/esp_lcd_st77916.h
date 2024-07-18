b2cf42be610a10834f4bcd79e4e96a301eecc0a1
姚建忠 <hunt-man@163.com>
2024-07-18 18:05:29 +0800

    添加st77916驱动

--- src/esp_lcd_st77916.h
+++ src/esp_lcd_st77916.h
@@ -13,34 +13,9 @@
 extern "C" {
 #endif
 
-/**
- * @brief LCD panel initialization commands.
- *
- */
-typedef struct {
-    int cmd;                /*<! The specific LCD command */
-    const void *data;       /*<! Buffer that holds the command specific data */
-    size_t data_bytes;      /*<! Size of `data` in memory, in bytes */
-    unsigned int delay_ms;  /*<! Delay in milliseconds after this command */
-} st77916_lcd_init_cmd_t;
-
-/**
- * @brief LCD panel vendor configuration.
- *
- * @note  This structure can be used to select interface type and override default initialization commands.
- * @note  This structure needs to be passed to the `vendor_config` field in `esp_lcd_panel_dev_config_t`.
- *
- */
-typedef struct {
-    const st77916_lcd_init_cmd_t *init_cmds;    /*!< Pointer to initialization commands array.
-                                                 *  The array should be declared as `static const` and positioned outside the function.
-                                                 *  Please refer to `vendor_specific_init_default` in source file
-                                                 */
-    uint16_t init_cmds_size;    /*<! Number of commands in above array */
-    struct {
-        unsigned int use_qspi_interface: 1;     /*<! Set to 1 if use QSPI interface, default is SPI interface */
-    } flags;
-} st77916_vendor_config_t;
+#define ESP_LCD_ST77916_VER_MAJOR    (0)
+#define ESP_LCD_ST77916_VER_MINOR    (0)
+#define ESP_LCD_ST77916_VER_PATCH    (2)
 
 /**
  * @brief Create LCD panel for model ST77916
