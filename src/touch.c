721058f81ef43b44113d20560b1d16582281949e
姚建忠 <hunt-man@163.com>
2024-07-20 01:16:20 +0800

    添加cst816s驱动

--- src/touch.c
+++ src/touch.c
@@ -37,7 +37,7 @@ static mp_obj_t lvgl_esp32_Touch_init(mp_obj_t self_ptr)
 {
     lvgl_esp32_Touch_obj_t *self = MP_OBJ_TO_PTR(self_ptr);
 
-    ESP_LOGI(TAG,"Initializing I2C for display touch");
+    ESP_LOGI(TAG,"Initializing I2C for display touch sda:%d,scl:%d",self->sda,self->scl);
     const i2c_config_t i2c_conf = {
             .mode = I2C_MODE_MASTER,
             .sda_io_num = self->sda,
@@ -46,8 +46,8 @@ static mp_obj_t lvgl_esp32_Touch_init(mp_obj_t self_ptr)
             .scl_pullup_en = GPIO_PULLUP_ENABLE,
             .master.clk_speed = 400000,
     };
-    ESP_ERROR_CHECK(i2c_param_config(I2C_NUM_0, &i2c_conf));
-    ESP_ERROR_CHECK(i2c_driver_install(I2C_NUM_0, i2c_conf.mode, 0, 0, 0));
+    ESP_ERROR_CHECK(i2c_param_config(self->ic2_num, &i2c_conf));
+    ESP_ERROR_CHECK(i2c_driver_install(self->ic2_num, i2c_conf.mode, 0, 0, 0));
     ESP_LOGI(TAG,"Initializing IO for display touch");
     esp_lcd_panel_io_i2c_config_t tp_io_config = ESP_LCD_TOUCH_IO_I2C_CST816S_CONFIG();
     ESP_ERROR_CHECK(esp_lcd_new_panel_io_i2c((esp_lcd_i2c_bus_handle_t)self->ic2_num, &tp_io_config, &self->tp_io_handle));
