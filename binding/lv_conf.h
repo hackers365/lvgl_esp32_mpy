5af4fa34a9b6b5c7ecb0fde755e48c2c27831dcc
姚建忠 <hunt-man@163.com>
2024-07-19 15:07:14 +0800

    添加st77916驱动

--- binding/lv_conf.h
+++ binding/lv_conf.h
@@ -676,7 +676,7 @@ extern void mp_lv_init_gc();
 /*File system interfaces for common APIs */
 
 /*API for fopen, fread, etc*/
-#define LV_USE_FS_STDIO 0
+#define LV_USE_FS_STDIO 1
 #if LV_USE_FS_STDIO
     #define LV_FS_STDIO_LETTER 'A'      /*Set an upper cased letter on which the drive will accessible (e.g. 'A')*/
     #define LV_FS_STDIO_PATH ""         /*Set the working directory. File/directory paths will be appended to it.*/
