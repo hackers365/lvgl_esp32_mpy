6956cc205228de1f3fb1e8d09e1d534acfddb9af
姚建忠 <hunt-man@163.com>
2024-07-26 13:45:32 +0800

    PNG解码

--- micropython.cmake
+++ micropython.cmake
@@ -31,4 +31,4 @@ target_include_directories(usermod_lvgl_esp32 INTERFACE
 target_link_libraries(usermod_lvgl_esp32 INTERFACE lvgl_interface)
 
 target_link_libraries(usermod INTERFACE usermod_lvgl_esp32)
-target_link_libraries(usermod /home/yaojz/data2/pnglib/lib/libpng.a)
+target_link_libraries(usermod INTERFACE /home/yaojz/data2/pnglib/lib/libpng.a)
