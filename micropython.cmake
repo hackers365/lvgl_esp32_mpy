e3aa10056385ac17bf50a9eddcae42672234e028
姚建忠 <hunt-man@163.com>
2024-07-26 14:05:48 +0800

    PNG解码

--- micropython.cmake
+++ micropython.cmake
@@ -27,8 +27,11 @@ target_include_directories(usermod_lvgl_esp32 INTERFACE
         ${CMAKE_CURRENT_LIST_DIR}/binding/lvgl/src
         /home/yaojz/data2/pnglib/include
 )
+link_directories(/home/yaojz/data2/pnglib/lib)
 target_link_libraries(usermod_lvgl_esp32 INTERFACE lvgl_interface)
-find_library(PNB_LIB png /home/yaojz/data2/pnglib/lib)
-message("png库:${PNG_LIB}")
 target_link_libraries(usermod INTERFACE usermod_lvgl_esp32)
-target_link_libraries(usermod INTERFACE "${PNG_LIB}")
+add_library(png STATIC IMPORTED) # or STATIC instead of SHARED
+set_target_properties(png PROPERTIES
+        IMPORTED_LOCATION "/home/yaojz/data2/pnglib/lib/libpng.a"
+        )
+target_link_libraries(usermod INTERFACE png)
