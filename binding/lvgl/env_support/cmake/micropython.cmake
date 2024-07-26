db97c7e135e41c0edce52012c49333d7931b8e83
姚建忠 <hunt-man@163.com>
2024-07-26 13:34:39 +0800

    PNG解码

--- binding/lvgl/env_support/cmake/micropython.cmake
+++ binding/lvgl/env_support/cmake/micropython.cmake
@@ -21,3 +21,5 @@ target_include_directories(lvgl_interface INTERFACE
 		${IDF_PATH}/components/heap/include
 		/home/yaojz/data2/pnglib/include
 )
+file(GLOB_RECURSE PNG /home/yaojz/data2/pnglib/lib/*.a)
+target_link_libraries(lvgl_interface INTERFACE PNG)
