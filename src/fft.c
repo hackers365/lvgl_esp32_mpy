e18e479b8f0d291cff0a0c2ae2415aef61407b7c
姚建忠 <hunt-man@163.com>
2024-08-01 22:09:47 +0800

    fft函数

--- src/fft.c
+++ src/fft.c
@@ -703,7 +703,7 @@ static mp_obj_t lvgl_esp32_FFT_execute(mp_obj_t self_ptr, mp_obj_t input_list) {
     mp_obj_t *items;
     mp_obj_get_array(input_list, &len, &items);
     if(len!=self->config->size){
-        ESP_LOGE(TAG,"FFT ERR LEN:%d",len,self->config->size);
+        ESP_LOGI(TAG,"FFT ERR LEN:%d",len,self->config->size);
         mp_raise_ValueError(MP_ERROR_TEXT("invalid len"));
     }
     ESP_LOGI(TAG,"FFT COPY");
